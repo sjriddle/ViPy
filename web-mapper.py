1dfcb20b607f399179d388a2fba9b485f0dceea1
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-04-25 09:04:58 -0600

    Update web-mapper.py

--- web-mapper.py
+++ web-mapper.py
@@ -19,23 +19,19 @@ for r,d,f in os.walk("."):
         if os.path.splitext(files)[1] not in filters:
             web_paths.put(remote_path)
 
-
 def test_remote():
     while not web_paths.empty(): 
         path = web_paths.get()
         url = "%s%s" % (target, path)
-        request = urllib2.Request(url) 
-
+        request = urllib2.Request(url)
         try:
             response = urllib2.urlopen(request)
             content  = response.read()
             print("[%d] => %s" % (response.code,path)) 
             response.close()
-        
         except urllib2.HTTPError as error: 
             print("Failed %s" % error.code)
             pass
-        
         
 for i in range(threads): 
     print("Spawning thread: %d" % i)
