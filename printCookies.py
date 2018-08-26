a77a259e3066dfd0765b7217dd15835fe815577e
xsrx <samuel.riddle1@gmail.com>
2018-08-26 16:45:35 -0600

    Fixed print cookie script

--- printCookies.py
+++ printCookies.py
@@ -1,14 +1,15 @@
+#!/usr/bin/python
 import mechanize
 import cookielib
 
 def printCookies(url):
     browser = mechanize.Browser()
     cookie_jar = cookielib.LWPCookieJar()
-    broser.set_cookiejar(cookie_jar)
+    browser.set_cookiejar(cookie_jar)
     page = browser.open(url)
 
     for cookie in cookie_jar:
         print(cookie)
 
-url = '{INSERT URL HERE}'
+url = 'https://{INSERT URL}'
 printCookies(url)
