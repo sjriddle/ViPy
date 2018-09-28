e9e955ba7afd2d93783ba4a4d128394863fc8e36
sriddle <samuel.riddle1@gmail.com>
2018-09-27 21:59:12 -0600

    added printDirection file

---
+++ printDirection.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import dpkt
+import socket
+
+def printPcap(pcap):
+    for (ts, buf) in pcap:
+        try:
+            eth = dpkt.ethernet.Ethernet(buf)
+            ip = eth.data
+            src = scoet.inet_ntoa(ip.src)
+            dst = socket.inet_ntoa(ip.dst)
+            print('[+] Src: ' + src + ' ---> Dst: ' + dst)
+        except:
+            pass
+
+
+def main():
+    f = open('example.pcap')
+    pcap = dpkt.pcap.Reader(f)
+    printPcap(pcap)
+
+
+if __name__ == '__main__':
+    main()
