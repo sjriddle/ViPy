5217763d04942774e8fc911aa117af6fc424ee01
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2019-09-26 09:58:20 -0600

    Update web-mapper.py

--- web-mapper.py
+++ web-mapper.py
@@ -7,8 +7,10 @@ threads = 10
 target = "http://www.test.com"
 directory = "/Users/<USERNAME>/Downloads/joomla3-1.1"
 filters = [".jpg", ".gif", "png", ".css"]
+
 os.chdir(directory)
 web_paths = Queue.Queue()
+
 for r,d,f in os.walk("."):
     for files in f:
         remote_path = "%s/%s" % (r,files)
