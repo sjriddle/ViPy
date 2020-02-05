e25bfbf1d144969229c1a54303ce7e0c66028534
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2020-02-05 11:36:44 -0700

    Update creditSniff.py

--- creditSniff.py
+++ creditSniff.py
@@ -8,11 +8,11 @@ def findCreditCard(pkt):
     masterRE = re.findall('5[1-5][0-9]{14}', raw)
     visaRE = re.findall('4[0-9]{12}(?:[0-9]{3})?', raw)
     if americaRE:
-        print('[+] Found American Express Card: ' + americaRE[0])
+        print(f'[+] Found American Express Card: {americaRE[0]}')
     if masterRE:
-        print('[+] Found MasterCard Card: ' + masterRE[0])
+        print(f'[+] Found MasterCard Card: {masterRE[0]}')
     if visaRE:
-        print('[+] Found Visa Card: ' + visaRE[0])
+        print(f'[+] Found Visa Card: {visaRE[0]}')
 
 def main():
     parser = optparse.OptionParser('usage %prof -i <interface>')
