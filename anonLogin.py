8d721d5a323972ec3c0ad2717db3d5180a51fb27
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2020-03-25 12:28:47 -0600

    Update anonLogin.py

--- anonLogin.py
+++ anonLogin.py
@@ -8,7 +8,7 @@ def anonLogin(hostname):
         ftp.quit()
         return True
     except:
-        print(f'\n[-] {(hostname)} FTP Anonymous Login Fail.')
+        print(f'\n[-] {str(hostname)} FTP Anonymous Login Fail.')
         return False
 
 host = '127.0.0.1'
