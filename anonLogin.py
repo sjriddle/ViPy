26b85a131aafe3ca8b1c5b8306796b355fb1d82c
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2020-03-25 12:28:32 -0600

    Update anonLogin.py

--- anonLogin.py
+++ anonLogin.py
@@ -4,11 +4,11 @@ def anonLogin(hostname):
     try:
         ftp = ftplib.FTP(hostname)
         ftp.login('anonymous', 'me@your.com')
-        print('\n[*] ' + str(hostname) + ' FTP Anonymous Login Success.')
+        print(f'\n[*] {str(hostname)} FTP Anonymous Login Success.')
         ftp.quit()
         return True
     except:
-        print('\n[-] ' + str(hostname) + ' FTP Anonymous Login Fail.')
+        print(f'\n[-] {(hostname)} FTP Anonymous Login Fail.')
         return False
 
 host = '127.0.0.1'
