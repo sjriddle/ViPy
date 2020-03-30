68657ebb778f0689a519ce3e6285f6c04396e62b
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2020-03-30 12:05:24 -0600

    Update sshCommand.py

--- sshCommand.py
+++ sshCommand.py
@@ -10,19 +10,19 @@ def send_command(child, cmd):
 
     
 def connect(user, host, password):
-    ssh_newkey = 'Are you sure you want to continue connecting'
+    ssh_newkey = 'Are you sure you want to continue connecting?'
     connStr = f'ssh {user}@{host}'
     child = pexpect.spawn(connStr)
     ret = child.expect([pexpect.TIMEOUT, ssh_newkey,'[P|p]assword:'])
     if ret == 0:
         print('[-] Error Connecting')
-        return
+        exit(1)
     if ret == 1:
         child.sendline('yes')
         ret = child.expect([pexpect.TIMEOUT, '[P|p]assword:'])
         if ret == 0:
             print('[-] Error Connecting')
-            return
+            exit(1)
     child.sendline(password)
     child.expect(PROMPT)
     return child
