d5e667102a3774ed19da846f873c0f8fb0e0ff26
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2020-04-16 10:49:17 -0600

    Update and rename geoIP.py to geo_ip.py

--- geo_ip.py
+++ geo_ip.py
@@ -10,7 +10,7 @@ def printRecord(tgt):
     lat = rec['latitude']
     
     print(f'[*] Target: {tgt} Geo-located.')
-    print(f'[+] {city}, {region}, {country}')
+    print(f'[+] Location: {city}, {region}, {country}')
     print(f'[+] Latitude: {lat}, Longitude: {long}')
 
 tgt = '127.0.0.1'
